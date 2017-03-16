99505dfa166fdca30d77e8206370155fc0f9f0e0
Shokry <shokry92@gmail.com>
2017-03-16 18:46:33 +0200

    Dummy popover for the mosaic part

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import '../css/Toolbar.css';
 
 class Toolbar extends Component {
+  constructor(props) {
+   super(props);
+
+   this.state = {active: false};
+
+   this.toggleActive = this.toggleActive.bind(this);
+ }
+
   render() {
     return (
       <div className="toolbar">
-        <span className="toolbar-item">Mosaic</span>
-        <span className="toolbar-item">Retro</span>
+        <div className="toolbar-item">
+          <a href="#" onClick={this.toggleActive}>Mosaic</a>
+          <div className={this.state.active? "popover popover-active": "popover"}>
+            <div className="inputContainer"><label>Width:</label> <input type="number" min="1"></input></div>
+          <div className="inputContainer"><label>Height:</label> <input type="number" min="1"></input></div>
+          <div><button>Go</button></div>
+          </div>
+        </div>
+        <div className="toolbar-item">Retro</div>
       </div>
     );
   }
+
+  toggleActive(e) {
+    e.preventDefault();
+    this.setState(prevState => ({active: !prevState.active}));
+  }
+
 }
 
 export default Toolbar;
