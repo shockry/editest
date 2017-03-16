b1a842c849976fcd64d3729cf4cd2d5a64e4b489
Shokry <shokry92@gmail.com>
2017-03-16 16:22:28 +0200

    Draw uploaded image to canvas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import './App.css';
 import Toolbar from './components/toolbar'
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+
+   this.triggerUpload = this.triggerUpload.bind(this);
+   this.drawImage = this.drawImage.bind(this);
+   // refs
+   this.imageInput = null;
+   this.canvas = null;
+ }
   render() {
     return (
       <div>
         <div className="topbar">
           <div className="left navbar-container">
-            <button type="button"> Upload </button>
+            <input className="hiddenInput" type="file"
+                   accept="image/*" id="image-picker"
+                   ref={input => {this.imageInput = input;}}
+                   onChange={this.drawImage}>
+            </input>
+            <button type="button" onClick={this.triggerUpload}> Upload </button>
           </div>
 
           <div className="middle navbar-container">
@@ -30,9 +44,39 @@ class App extends Component {
         </div>
 
         <Toolbar></Toolbar>
+        <div className="preview">
+          <canvas className="drawingArea"
+                  ref={canvas => {this.canvas = canvas;}}>
+          </canvas>
+        </div>
       </div>
     );
   }
+
+  triggerUpload() {
+    this.imageInput.click();
+  }
+
+  drawImage(e) {
+    const file = this.imageInput.files[0];
+    const imageType = /^image\//;
+    const isImage = file && imageType.test(file.type);
+
+    if (this.imageInput.files.length === 1 && isImage) {
+      const canvas = this.canvas;
+
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+
+      img.onload = function() {
+        URL.revokeObjectURL(this.src);
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+        canvas.getContext('2d').drawImage(this, 0, 0);
+      };
+    }
+  }
+
 }
 
 export default App;
