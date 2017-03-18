26927da34a9f8df92d5ada4dd7a2901dc93aca16
Shokry <shokry92@gmail.com>
2017-03-18 18:30:00 +0200

    Duplicate popover markup and some new styles

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -7,9 +7,10 @@ class Toolbar extends Component {
   constructor(props) {
    super(props);
 
-   this.state = {active: false};
+   this.state = {active: false, retroActive: false};
 
    this.toggleActive = this.toggleActive.bind(this);
+   this.toggleRetroActive = this.toggleRetroActive.bind(this);
  }
 
   render() {
@@ -19,15 +20,23 @@ class Toolbar extends Component {
           <a href="#" onClick={this.toggleActive}>Mosaic</a>
           <div className={this.state.active? "popover popover-active": "popover"}>
             <div className="inputContainer">
-              <label>Width:</label> <input type="number" min="1"></input>
+              <label>Width</label> <input type="number" min="1"></input>
             </div>
             <div className="inputContainer">
-              <label>Height:</label> <input type="number" min="1"></input>
+              <label>Height</label> <input type="number" min="1"></input>
             </div>
             <button>Go</button>
           </div>
         </div>
-        <div className="toolbar-item">Retro</div>
+        <div className="toolbar-item">
+          <a href="#" onClick={this.toggleRetroActive}>Retro</a>
+          <div className={this.state.retroActive? "popover popover-active": "popover"}>
+            <div className="inputContainer">
+              <label>Intensity</label> <input type="range" min="1"></input>
+            </div>
+            <button>Go</button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -35,6 +44,11 @@ class Toolbar extends Component {
   toggleActive(e) {
     e.preventDefault();
     this.setState(prevState => ({active: !prevState.active}));
+  }
+
+  toggleRetroActive(e) {
+    e.preventDefault();
+    this.setState(prevState => ({retroActive: !prevState.retroActive}));
   }
 
 }
