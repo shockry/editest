78b28a80ddd7d960b6e08b7966bff0197d21ccc0
Shokry <shokry92@gmail.com>
2017-03-28 04:24:56 +0200

    Download canvas as a blob instead of object url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Topbar from './components/Topbar';
 import { setVars } from './utils/sharedVars';
-
+import { setCanvasToBlob } from './utils/canvasToBlob';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +15,7 @@ class App extends Component {
 
  componentDidMount() {
    this.setState({canvas: this.canvas});
+   setCanvasToBlob(); // A polyfill for canvas.toBlob if not present
    setVars({canvas: this.canvas});
  }
 
--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -6,13 +6,16 @@ class DownloadButton extends Component {
    super(props);
 
    this.downloadImage = this.downloadImage.bind(this);
+   this.downloadCanvasBlob = this.downloadCanvasBlob.bind(this);
+   this.revokeURL = this.revokeURL.bind(this);
    // refs
    this.imagedownloader = null;
  }
   render() {
     return (
       <div className="right">
-        <a className="hiddenInput" ref={input => {this.imagedownloader = input;}}/>
+        <a className="hiddenInput" ref={input => {this.imagedownloader = input;}}
+          onClick={this.revokeURL}/>
         <button type="button" onClick={this.downloadImage}>
           <img className="button-icon" src={downloadIcon} alt="upload icon"/>
           <span className="button-text">Download</span>
@@ -23,11 +26,20 @@ class DownloadButton extends Component {
 
   downloadImage(e) {
     if (this.props.canvas.hasImage) {
-      const data = this.props.canvas.toDataURL();
-      this.imagedownloader.href = data;
-      this.imagedownloader.download = "bestest image";
-      this.imagedownloader.click();
+      this.props.canvas.toBlob(this.downloadCanvasBlob);
     }
+  }
+
+  downloadCanvasBlob(blob) {
+    this.imagedownloader.href = URL.createObjectURL(blob);
+    this.imagedownloader.download = "bestest image";
+    this.imagedownloader.click();
+  }
+
+  revokeURL() {
+    // requestAnimationFrame makes the browser prepare the file for download
+    // before revoking the URL
+    requestAnimationFrame(() => URL.revokeObjectURL(this.imagedownloader.href));
   }
 
 }
